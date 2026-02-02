c20d2bde564d5660ed507e2b6c3998a74362279d
MattiaFilo <filipponimattia805@gmail.com>
2026-02-02 17:53:48 +0100

    Removed docs from repo

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,6 @@ int main(int argc, char** argv) {
     unsigned int seed = 1111; // seed for cluster initialization
     int max_iters = 2000; // max number of iterations
 
-    //kMeans(&iris, n_points, n_clusters, 300, seed, "../outputs/results_sequential_iris_" + std::to_string(seed) + ".csv");
     kMeans(&wineQuality, n_points, n_clusters, max_iters, seed, "../outputs/results_sequential_wine_replicated_" + std::to_string(seed) + "_" + std::to_string(max_iters) + ".csv");
 
     return 0;
